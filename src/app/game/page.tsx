318052915d8f7de6b8412cee26953a4c3741b4ff
Jinhyuk-P <A01286288@tec.mx>
2025-04-27 11:45:16 -0600

    page.tsx

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,11 +1,25 @@
-'use client'
-import Sidebar from "@/components/sidebar/page";
-import styles from "./page.module.css";
+"use client";
+import { useEffect } from "react";
 
-export default function Home() {
+export default function GamePage() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "/unityBuild/Build/UnityLoader.js"; // UnityLoader file
+    script.onload = () => {
+      window.createUnityInstance(document.querySelector("#unity-canvas"), {
+        dataUrl: "/unityBuild/Build/yourbuild.data",
+        frameworkUrl: "/unityBuild/Build/yourbuild.framework.js",
+        codeUrl: "/unityBuild/Build/yourbuild.wasm",
+      }).catch((message) => {
+        console.error(message);
+      });
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
-     <div className={styles.container}>
-          <Sidebar />
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <canvas id="unity-canvas" style={{ width: "960px", height: "600px" }}></canvas>
     </div>
   );
 }
